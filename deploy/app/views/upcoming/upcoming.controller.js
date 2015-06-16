171e02ca4d2ec70fa0849fe6947900346e1799cc
shikhaj <shikha.jain@imaginea.com>
2015-06-16 11:09:09 +0530

    remove timestamp, remove unwanted files

--- deploy/app/views/upcoming/upcoming.controller.js
+++ deploy/app/views/upcoming/upcoming.controller.js
@@ -1,26 +1,15 @@
 'use strict';
 
 angular.module('leaveTrackerAppApp')
-  .controller('UpcomingCtrl', function ($scope, holidayFactory, Scopes,$location) {
-
-  console.log('loading upcoming controller');
-   Scopes.store('UpcomingCtrl', $scope);
-    $scope.message = 'Hello';
-    if(!Scopes.get('UpcomingRecordList'))
-    { 
-    	window.holidayLoadFlag = 0;
-	    holidayFactory.getUpcomingDetails().then(function(response) {
-	        $scope.upcomingRecord = response;
-	        Scopes.store('UpcomingRecordList', $scope.upcomingRecord);
-
-	    });
- 	}else{
- 		$scope.upcomingRecord = Scopes.get('UpcomingRecordList');
- 	}
-
-     $scope.addrecord = function(getrecord){
-     	$scope.upcomingRecord.push(getrecord);
-     	console.log($scope.upcomingRecord.length); 
-     	
-     };
+  .controller('UpcomingCtrl', function($scope, holidayFactory, Scopes, $location) {
+    Scopes.store('UpcomingCtrl', $scope);
+    if (!Scopes.get('UpcomingRecordList')) {
+        window.holidayLoadFlag = 0;
+        holidayFactory.getUpcomingDetails().then(function(response) {
+            $scope.upcomingRecord = response;
+            Scopes.store('UpcomingRecordList', $scope.upcomingRecord);
+        });
+    }else {
+        $scope.upcomingRecord = Scopes.get('UpcomingRecordList');
+    }
   });
